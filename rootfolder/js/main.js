8a224e1cf239b1ef5ca1d08d78fc8082e45489d9
Abhishek Singh <42760946+abhisheks802@users.noreply.github.com>
2020-07-13 02:49:09 +0530

    Add files via upload

---
+++ rootfolder/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $(".img1").mouseover(function () {
+        $(this).hide();
+        $(".img2").fadeIn(600);
+    });
+    $(".img2").mouseleave(function () {
+        $(this).hide();
+        $(".img1").fadeIn(600);
+    });
+    $(".dropdown").click(function () {
+        $(".menu-box").slideToggle();
+    });
+    if($(window).width() < 1025) {
+        $(".menu li").click(function () {
+            $(".menu-box").slideUp();
+        });
+    }
+    $("#read").click(function () {
+        $(".dots").fadeToggle();
+        $(".other-words").fadeToggle();
+    });
+    $(".cart").on("click", function () {
+        $(this).closest(".img-price")
+            .find("img")
+            .clone()
+            .addClass("zoom")
+            .appendTo(".nav-bar");
+        setTimeout(function () {
+            $(".zoom").remove();
+        },1000);
+    });
+    $("#come-top").click(function () {
+        window.location.href = "#products";
+    });
+
+});
